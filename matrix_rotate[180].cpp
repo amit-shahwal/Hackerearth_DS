6011d3862d68f3fdfc26f65d92ad76923c237d9d
amit-shahwal <amitshahwal24@gmail.com>
2020-09-27 14:23:25 +0530

    matrix_rotate[180].cpp

---
+++ matrix_rotate[180].cpp
@@ -0,0 +1,51 @@
+//this program is to roate matrix clock wise or anti cockwise
+#include<bits/stdc++.h>
+#define R 3
+#define C 3
+using namespace std;
+void transpose(int arr[R][C])
+{
+    for(int i=0; i<R; i++)
+        for(int j=i; j<C; j++)
+            swap(arr[i][j],arr[j][i]);
+
+}
+void columnrevrse(int arr[R][C])
+{
+    for(int i=0; i<C; i++)
+        for(int j=0,k=C-1; j<k;j++,k--)
+            swap(arr[j][i],arr[k][i]);
+
+}
+
+void printmatrix(int arr[R][C])
+{
+    for(int i=0; i<R; i++){
+        for(int j=0; j<C; j++)
+            cout<<arr[i][j]<<" ";
+    cout<<endl;
+}}
+
+void rowreverse(int arr[R][C])
+{
+    for(int i=0;i<R;i++)
+        for(int j=0,k=R-1;j<k;j++,k--)
+        swap(arr[i][j],arr[i][k]);
+
+}
+void rotatemat(int arr[R][C])
+{
+    rowreverse(arr);
+    columnrevrse(arr);
+   // transpose(arr);
+
+}
+int main()
+{
+
+  int arr[R][C] = {{1,2,3},{4,5,6},{7,8,9}};
+rotatemat(arr);
+printmatrix(arr);
+
+}
+
